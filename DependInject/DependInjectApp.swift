6fa16e61877716440cfd0c111fa6837a54932b62
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-06-01 17:18:15 +0200

    Initial Commit

---
+++ DependInject/DependInjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependInjectApp.swift
+//  DependInject
+//
+//  Created by Maziar Layeghkar on 01.06.24.
+//
+
+import SwiftUI
+
+@main
+struct DependInjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
